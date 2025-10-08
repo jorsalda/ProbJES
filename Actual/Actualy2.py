c7bc43f722ce30f17dee1d15efabbc4833db1e44
JORGE <jorsalda@gmail.com>
2025-10-08 08:50:45 -0500

    todo

---
+++ Actual/Actualy2.py
@@ -0,0 +1,6 @@
+def datosJES():
+    nombre = input("Ingresa tu nombre: ")
+    edad = int(input("Ingresa tu edad: "))  # convierte el texto a número entero
+    print(f"Hola {nombre}, tienes {edad} años.")
+
+datosJES()
